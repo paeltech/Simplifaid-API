d625bc0c9b4d28658f779d2631827f7eb6b48fc9
Paul Mandele <Paul@Pauls-Macbook-Pro.local>
2020-05-26 07:30:14 +0300

    Updated pgsql db config

--- config/database.php
+++ config/database.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Str; 
+
 $DATABASE_URL=parse_url('postgres://grnpgzotlmlday:6f47b82b1951eb36d1eac10e785dd8c4de1140770d4bdfe73475bfbe5d756980@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/d3felics3s2la5');
 
-use Illuminate\Support\Str;
 
 return [
 
@@ -48,11 +49,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => $DATABASE_URL,
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["/path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
